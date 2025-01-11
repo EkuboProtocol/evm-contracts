4a40cee309972342ca83709a6f36ab8e2d1b72b6
Moody Salem <moody.salem@gmail.com>
2025-01-11 17:06:10 -0500

    refactor the owned upgradeable stuff since it will be reused

--- src/Core.sol
+++ src/Core.sol
@@ -1,29 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.28;
 
-import {Ownable} from "solady/auth/Ownable.sol";
-import {UUPSUpgradeable} from "solady/utils/UUPSUpgradeable.sol";
+import {OwnedUpgradeable} from "./base/OwnedUpgradeable.sol";
 
-contract Core is Ownable, UUPSUpgradeable {
-    // Only called once at deploy time
-    function initialize(address owner) external {
-        _initializeOwner(owner);
-    }
-
-    // Prevents initialize from being called more than once
-    function _guardInitializeOwner() internal pure override returns (bool guard) {
-        guard = true;
-    }
-
-    // This proxy can only be upgraded by the owner
-    function _authorizeUpgrade(address) internal override onlyOwner {}
-
-    function getImplementation() external view returns (address implementation) {
-        assembly {
-            implementation := sload(_ERC1967_IMPLEMENTATION_SLOT)
-        }
-    }
-
+contract Core is OwnedUpgradeable {
     // The entrypoint for all operations on the core contract
     function lock(bytes calldata data) external onlyProxy {}
 }

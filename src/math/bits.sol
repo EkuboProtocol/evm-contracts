2859815e781bfc78bb3be687e21fd80707fcfa84
Moody Salem <moody.salem@gmail.com>
2025-01-12 22:16:15 -0500

    use solady msb impl

--- src/math/bits.sol
+++ src/math/bits.sol
@@ -1,44 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.28;
 
+import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+
 error MsbNonZero();
 
 function msb(uint256 x) pure returns (uint8 res) {
     if (x == 0) revert MsbNonZero();
 
-    assembly ("memory-safe") {
-        let s := mul(128, eq(lt(x, 0x100000000000000000000000000000000), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(64, eq(lt(x, 0x10000000000000000), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(32, eq(lt(x, 0x100000000), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(16, eq(lt(x, 0x10000), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(8, eq(lt(x, 0x100), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(4, eq(lt(x, 0x10), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(2, eq(lt(x, 0x4), 0))
-        x := shr(s, x)
-        res := add(s, res)
-
-        s := mul(1, eq(lt(x, 0x2), 0))
-        x := shr(s, x)
-        res := add(s, res)
-    }
+    res = uint8(FixedPointMathLib.log2(x));
 }
 
 error LsbNonZero();

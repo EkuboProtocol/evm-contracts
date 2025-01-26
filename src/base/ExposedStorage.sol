9f82ca6c55e0272a3d3d82d33a9f7d508a6662ac
Moody Salem <moody.salem@gmail.com>
2025-01-26 08:31:41 -0500

    small opt to exposed storage

--- src/base/ExposedStorage.sol
+++ src/base/ExposedStorage.sol
@@ -4,15 +4,17 @@ pragma solidity =0.8.28;
 import {IExposedStorage} from "../interfaces/IExposedStorage.sol";
 
 abstract contract ExposedStorage is IExposedStorage {
-    function sload(bytes32 slot) external view returns (bytes32 result) {
+    function sload(bytes32 slot) external view returns (bytes32) {
         assembly ("memory-safe") {
-            result := sload(slot)
+            mstore(0, sload(slot))
+            return(0, 32)
         }
     }
 
-    function tload(bytes32 slot) external view returns (bytes32 result) {
+    function tload(bytes32 slot) external view returns (bytes32) {
         assembly ("memory-safe") {
-            result := tload(slot)
+            mstore(0, tload(slot))
+            return(0, 32)
         }
     }
 }

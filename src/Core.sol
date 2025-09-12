0ae6920e2f9eb0e1107ad741a5c79b39d06881d5
Moody Salem <moody.salem@gmail.com>
2025-09-12 16:07:02 -0400

    add comment

--- src/Core.sol
+++ src/Core.sol
@@ -575,6 +575,8 @@ contract Core is ICore, FlashAccountant, Ownable, ExposedStorage {
             assembly ("memory-safe") {
                 let o := mload(0x40)
                 mstore(o, shl(96, locker))
+                // note we only emit the first 16 bytes of the pool ID because it's overwritten in the next mstore, 16 bytes * 8 gas = 128 gas saved per swap
+                // off chain, the indexers must match by prefix. in case of a collision, this can cause swaps to be misattributed, but a collision is very unlikely.
                 mstore(add(o, 20), poolId)
                 mstore(add(o, 36), or(shl(128, delta0), and(delta1, 0xffffffffffffffffffffffffffffffff)))
                 mstore(add(o, 68), shl(128, liquidity))

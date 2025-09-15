a204637b536e259f699892038bbbdeb26962b571
Moody Salem <moody.salem@gmail.com>
2025-09-15 16:35:32 -0400

    add back refundNativeToken which is required for the positions and orders contract

--- src/base/PayableMulticallable.sol
+++ src/base/PayableMulticallable.sol
@@ -2,9 +2,17 @@
 pragma solidity =0.8.28;
 
 import {Multicallable} from "solady/utils/Multicallable.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
 abstract contract PayableMulticallable is Multicallable {
     function multicall(bytes[] calldata data) public payable override returns (bytes[] memory) {
         _multicallDirectReturn(_multicall(data));
+    }
+
+    // Allows a caller to refund any ETH sent to this contract for purpose of transient payments
+    function refundNativeToken() external payable {
+        if (address(this).balance != 0) {
+            SafeTransferLib.safeTransferETH(msg.sender, address(this).balance);
+        }
     }
 }

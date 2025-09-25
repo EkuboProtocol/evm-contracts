5fcb6d838dbb25e9f955a38bbae15a4ca874831f
Moody Salem <moody.salem@gmail.com>
2025-09-25 12:03:18 -0400

    move the code for fetching the current order status to TWAMMLib

--- src/libraries/TWAMMLib.sol
+++ src/libraries/TWAMMLib.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: Ekubo-DAO-SRL-1.0
 pragma solidity =0.8.30;
 
+import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+
 import {ITWAMM} from "../interfaces/extensions/ITWAMM.sol";
 import {ExposedStorageLib} from "./ExposedStorageLib.sol";
 import {TwammPoolState} from "../types/twammPoolState.sol";
 import {OrderState} from "../types/orderState.sol";
+import {OrderKey} from "../types/orderKey.sol";
+import {PoolKey} from "../types/poolKey.sol";
 import {PoolId} from "../types/poolId.sol";
+import {computeAmountFromSaleRate, computeRewardAmount} from "../math/twamm.sol";
 
-// Common storage getters we need for external contracts are defined here instead of in the core contract
+/// @title TWAMM Library
+/// @notice Helper methods for interacting with the TWAMM extension
 library TWAMMLib {
     using ExposedStorageLib for *;
 
@@ -61,5 +67,62 @@ library TWAMMLib {
         }
 
         return uint256(twamm.sload(key));
+    }
+
+    function executeVirtualOrdersAndGetCurrentOrderInfo(
+        ITWAMM twamm,
+        address owner,
+        bytes32 salt,
+        OrderKey memory orderKey
+    ) internal returns (uint112 saleRate, uint256 amountSold, uint256 remainingSellAmount, uint128 purchasedAmount) {
+        unchecked {
+            PoolKey memory poolKey = orderKey.toPoolKey(address(twamm));
+            twamm.lockAndExecuteVirtualOrders(poolKey);
+
+            uint32 lastUpdateTime;
+            bytes32 orderId = orderKey.toOrderId();
+
+            (lastUpdateTime, saleRate, amountSold) = orderState(twamm, owner, salt, orderId).parse();
+
+            uint256 _rewardRateSnapshot = rewardRateSnapshot(twamm, owner, salt, orderId);
+
+            if (saleRate != 0) {
+                uint256 rewardRateInside = twamm.getRewardRateInside(
+                    poolKey.toPoolId(), orderKey.startTime, orderKey.endTime, orderKey.sellToken < orderKey.buyToken
+                );
+
+                purchasedAmount = computeRewardAmount(rewardRateInside - _rewardRateSnapshot, saleRate);
+
+                if (block.timestamp > orderKey.startTime) {
+                    uint32 secondsSinceLastUpdate = uint32(block.timestamp) - lastUpdateTime;
+
+                    uint32 secondsSinceOrderStart = uint32(block.timestamp - orderKey.startTime);
+
+                    uint32 totalOrderDuration = uint32(orderKey.endTime - orderKey.startTime);
+
+                    uint32 remainingTimeSinceLastUpdate = uint32(orderKey.endTime) - lastUpdateTime;
+
+                    uint32 saleDuration = uint32(
+                        FixedPointMathLib.min(
+                            remainingTimeSinceLastUpdate,
+                            FixedPointMathLib.min(
+                                FixedPointMathLib.min(secondsSinceLastUpdate, secondsSinceOrderStart),
+                                totalOrderDuration
+                            )
+                        )
+                    );
+
+                    amountSold +=
+                        computeAmountFromSaleRate({saleRate: saleRate, duration: saleDuration, roundUp: false});
+                }
+                if (block.timestamp < orderKey.endTime) {
+                    remainingSellAmount = computeAmountFromSaleRate({
+                        saleRate: saleRate,
+                        duration: uint32(orderKey.endTime - FixedPointMathLib.max(orderKey.startTime, block.timestamp)),
+                        roundUp: true
+                    });
+                }
+            }
+        }
     }
 }

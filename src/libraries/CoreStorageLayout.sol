168fbfb4a6d4e605c52e5302da35417c6538243b
die-herdplatte <173669014+die-herdplatte@users.noreply.github.com>
2025-10-04 14:45:43 +0200

    Use random offsets for storage layouts & remove poolId from positions storage slot computation

--- src/libraries/CoreStorageLayout.sol
+++ src/libraries/CoreStorageLayout.sol
@@ -10,14 +10,24 @@ import {PositionId} from "../types/positionId.sol";
 ///      For certain storage values, the pool id is used as a base offset and
 ///      we allocate the following relative offsets (starting from the pool id) as:
 ///        0: pool state
-///        1 + 2: fees per liquidity
+///        [FPL_OFFSET, FPL_OFFSET + 1]: fees per liquidity
 ///        [TICKS_OFFSET + MIN_TICK, TICKS_OFFSET + MAX_TICK]: tick info
-///        [FPL_OUTSIDE_OFFSET + MIN_TICK, 2 * FPL_OUTSIDE_OFFSET + MAX_TICK]: fees per liquidity outside
+///        [FPL_OUTSIDE_OFFSET_VALUE0 + MIN_TICK, FPL_OUTSIDE_OFFSET_VALUE0 + MAX_TICK]: fees per liquidity outside (value0)
+///        [FPL_OUTSIDE_OFFSET_VALUE0 + FPL_OUTSIDE_OFFSET_VALUE1 + MIN_TICK, FPL_OUTSIDE_OFFSET_VALUE0 + FPL_OUTSIDE_OFFSET_VALUE1 + MAX_TICK]: fees per liquidity outside (value1)
 ///        [BITMAPS_OFFSET + FIRST_BITMAP_WORD, BITMAPS_OFFSET + LAST_BITMAP_WORD]: tick bitmaps
 library CoreStorageLayout {
-    uint256 internal constant TICKS_OFFSET = 0xffffffff;
-    uint256 internal constant FPL_OUTSIDE_OFFSET = 0xffffffffff;
-    uint256 internal constant BITMAPS_OFFSET = 0xffffffffffff;
+    /// @dev Generated using: cast keccak "TWAMMStorageLayout#FPL_OFFSET"
+    uint256 internal constant FPL_OFFSET = 0x48ed53dbfefe9728fce02ee791b95497bc6a2145a1e35f74f2764c959e0a5dc7;
+    /// @dev Generated using: cast keccak "TWAMMStorageLayout#TICKS_OFFSET"
+    uint256 internal constant TICKS_OFFSET = 0x307e36e3ff015b5b6fb70d03c03496fc324779dad7c9b54607b7ee976404dd49;
+    /// @dev Generated using: cast keccak "TWAMMStorageLayout#FPL_OUTSIDE_OFFSET_VALUE0"
+    uint256 internal constant FPL_OUTSIDE_OFFSET_VALUE0 =
+        0xb1a8425715766b889dad6ccf988bb924d91462fef123f57ce146305570eb0952;
+    /// @dev Generated using: cast keccak "TWAMMStorageLayout#FPL_OUTSIDE_OFFSET_VALUE1"
+    uint256 internal constant FPL_OUTSIDE_OFFSET_VALUE1 =
+        0xc777bb055b17730cca67e002e0fe1e9dd7f1a0a826671806a02ee8478998ce9d;
+    /// @dev Generated using: cast keccak "TWAMMStorageLayout#BITMAPS_OFFSET"
+    uint256 internal constant BITMAPS_OFFSET = 0x9ea4c5985e5123cf340a963f6e6e616537bf85b21e4f341bee18afd5d22512a0;
 
     /// @notice Computes the storage slot containing information on whether an extension is registered
     /// @param extension The extension address to check
@@ -42,7 +52,7 @@ library CoreStorageLayout {
     /// @return firstSlot The first of two consecutive storage slots in the Core contract
     function poolFeesPerLiquiditySlot(PoolId poolId) internal pure returns (bytes32 firstSlot) {
         assembly ("memory-safe") {
-            firstSlot := add(poolId, 1)
+            firstSlot := add(poolId, FPL_OFFSET)
         }
     }
 
@@ -67,8 +77,8 @@ library CoreStorageLayout {
         returns (bytes32 firstSlot, bytes32 secondSlot)
     {
         assembly ("memory-safe") {
-            firstSlot := add(poolId, add(FPL_OUTSIDE_OFFSET, tick))
-            secondSlot := add(firstSlot, FPL_OUTSIDE_OFFSET)
+            firstSlot := add(poolId, add(FPL_OUTSIDE_OFFSET_VALUE0, tick))
+            secondSlot := add(firstSlot, FPL_OUTSIDE_OFFSET_VALUE1)
         }
     }
 
@@ -92,9 +102,8 @@ library CoreStorageLayout {
         returns (bytes32 firstSlot)
     {
         assembly ("memory-safe") {
-            mstore(0, poolId)
-            mstore(32, positionId)
-            firstSlot := add(keccak256(0, 64), owner)
+            mstore(0, positionId)
+            firstSlot := add(keccak256(0, 32), add(poolId, owner))
         }
     }
 

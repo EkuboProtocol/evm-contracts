ac1858de40ae036907677ee32a3998336fcbbfa5
Moody Salem <moody.salem@gmail.com>
2025-10-20 12:32:37 -0400

    implement in bitmap with todo

--- src/types/bitmap.sol
+++ src/types/bitmap.sol
@@ -20,23 +20,27 @@ function isSet(Bitmap bitmap, uint8 index) pure returns (bool yes) {
 }
 
 // Returns the index of the most significant bit that is set _and_ less or equally significant to index, or 256 if no such bit exists.
-function leSetBit(Bitmap bitmap, uint8 index) pure returns (uint256) {
+function leSetBit(Bitmap bitmap, uint8 index) pure returns (uint256 v) {
     unchecked {
-        uint256 masked;
         assembly ("memory-safe") {
-            masked := and(bitmap, sub(shl(add(index, 1), 1), 1))
+            let masked := and(bitmap, sub(shl(add(index, 1), 1), 1))
+            v := sub(255, clz(masked))
+            // todo: we should flip the direction that we count so we don't have to do the sub
+            if eq(v, not(0)) {
+                v := 256
+            }
         }
-        return LibBit.fls(masked);
     }
 }
 
 // Returns the index of the least significant bit that is set _and_ more or equally significant to index, or 256 if no such bit exists.
-function geSetBit(Bitmap bitmap, uint8 index) pure returns (uint256) {
-    unchecked {
-        uint256 masked;
-        assembly ("memory-safe") {
-            masked := and(bitmap, not(sub(shl(index, 1), 1)))
+function geSetBit(Bitmap bitmap, uint8 index) pure returns (uint256 v) {
+    assembly ("memory-safe") {
+        let masked := and(bitmap, not(sub(shl(index, 1), 1)))
+        v := sub(255, clz(and(masked, sub(0, masked))))
+        // todo: we should flip the direction that we count so we don't have to do the sub
+        if eq(v, not(0)) {
+            v := 256
         }
-        return LibBit.ffs(masked);
     }
 }

9260bbcf238ce4dddfe95664a74639ca18d3f3d9
Moody Salem <moody.salem@gmail.com>
2025-12-09 09:01:40 -0500

    fix the view computation of position fees for stableswap pools

--- src/base/BasePositions.sol
+++ src/base/BasePositions.sol
@@ -61,7 +61,7 @@ abstract contract BasePositions is IPositions, UsesCore, PayableMulticallable, B
 
         (principal0, principal1) = (uint128(-delta0), uint128(-delta1));
 
-        FeesPerLiquidity memory feesPerLiquidityInside = poolKey.config.isFullRange()
+        FeesPerLiquidity memory feesPerLiquidityInside = poolKey.config.isStableswap()
             ? CORE.getPoolFeesPerLiquidity(poolId)
             : CORE.getPoolFeesPerLiquidityInside(poolId, tickLower, tickUpper);
         (fees0, fees1) = position.fees(feesPerLiquidityInside);

81c2c2642afe321e7f5d7de70f8b3be18f6f80b3
Moody Salem <moody.salem@gmail.com>
2026-01-03 10:22:20 -0500

    also print the init code

--- script/PrintMEVCaptureRouterInitCodeHash.sol
+++ script/PrintMEVCaptureRouterInitCodeHash.sol
@@ -7,19 +7,20 @@ import {console} from "forge-std/console.sol";
 
 contract PrintMEVCaptureRouterInitCodeHash is Script {
     function run() public pure {
-        console.log("MEVCaptureRouter init code hash: ");
-        console.logBytes32(
-            keccak256(
-                abi.encodePacked(
-                    type(MEVCaptureRouter).creationCode,
-                    abi.encode(
-                        // Core =
-                        address(0x00000000000014aA86C5d3c41765bb24e11bd701),
-                        // MEVCapture =
-                        address(0x5555fF9Ff2757500BF4EE020DcfD0210CFfa41Be)
-                    )
-                )
+        bytes memory initCode = abi.encodePacked(
+            type(MEVCaptureRouter).creationCode,
+            abi.encode(
+                // Core =
+                address(0x00000000000014aA86C5d3c41765bb24e11bd701),
+                // MEVCapture =
+                address(0x5555fF9Ff2757500BF4EE020DcfD0210CFfa41Be)
             )
         );
+
+        console.log("Initcode");
+        console.logBytes(initCode);
+
+        console.log("MEVCaptureRouter init code hash: ");
+        console.logBytes32(keccak256(initCode));
     }
 }

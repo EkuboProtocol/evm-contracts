665e8333e550003b68a94d8482cc9fda438a2bf1
Moody Salem <moody.salem@gmail.com>
2026-02-03 12:52:22 -0500

    restore files that accidentally went missing in past commits

---
+++ test/MEVCaptureRouter.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: ekubo-license-v1.eth
+pragma solidity =0.8.33;
+
+import {FullTest} from "./FullTest.sol";
+import {RouterTest} from "./Router.t.sol";
+import {BaseMEVCaptureTest} from "./extensions/MEVCapture.t.sol";
+
+// makes sure all the same tests pass for the mev router
+contract MEVCaptureRouterTest is RouterTest, BaseMEVCaptureTest {
+    function setUp() public override(BaseMEVCaptureTest, FullTest) {
+        BaseMEVCaptureTest.setUp();
+    }
+
+    function coolAllContracts() internal virtual override(FullTest, BaseMEVCaptureTest) {
+        BaseMEVCaptureTest.coolAllContracts();
+    }
+}

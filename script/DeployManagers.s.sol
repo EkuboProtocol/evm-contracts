fa7f5685102aa243d9a8efe9f2d772e9cb03f4a4
Moody Salem <moody.salem@gmail.com>
2026-04-24 12:53:05 -0400

    fix the expected addresses

--- script/DeployManagers.s.sol
+++ script/DeployManagers.s.sol
@@ -21,7 +21,7 @@ contract DeployManagers is Script {
 
         bytes32 salt = vm.envOr("SALT", bytes32(0x28f4114b40904ad1cfbb42175a55ad64187c1b299773bd6318baa292375cf0dd));
         ICore core = ICore(payable(vm.envOr("CORE_ADDRESS", payable(0x00000000000014aA86C5d3c41765bb24e11bd701))));
-        ITWAMM twamm = ITWAMM(vm.envOr("TWAMM_ADDRESS", address(0xd4F1060cB9c1A13e1d2d20379b8aa2cF7541eD9b)));
+        ITWAMM twamm = ITWAMM(vm.envOr("TWAMM_ADDRESS", address(0xd47f1B1eDCfEaBb08F6eBd8FC337c27E636C75BA)));
 
         string memory positionsBaseUrl = _envStringOr("POSITIONS_BASE_URL", "https://prod-api.ekubo.org/positions/");
         string memory ordersBaseUrl = _envStringOr("ORDERS_BASE_URL", "https://prod-api.ekubo.org/orders/");

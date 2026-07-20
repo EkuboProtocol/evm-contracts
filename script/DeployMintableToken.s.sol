7b8b0a070e2f675a99fefdceb8a324c79790b089
Moody Salem <moody.salem@gmail.com>
2026-07-20 12:37:56 -0400

    Print mintable token init code hash

--- script/DeployMintableToken.s.sol
+++ script/DeployMintableToken.s.sol
@@ -2,14 +2,14 @@
 pragma solidity =0.8.33;
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 import {MintableERC20} from "../src/MintableERC20.sol";
 import {deployIfNeeded} from "./DeployAll.s.sol";
 
 /// @title DeployMintableToken
 /// @notice Deterministically deploys a mintable token owned by the broadcasting wallet.
 contract DeployMintableToken is Script {
-    bytes32 internal constant DEFAULT_DEPLOYMENT_SALT =
-        0x28f4114b40904ad1cfbb42175a55ad64187c1b299773bd6318baa292375cf0dd;
+    bytes32 internal constant DEFAULT_DEPLOYMENT_SALT = bytes32(0);
 
     error InvalidTokenDecimals(uint256 decimals);
 
@@ -22,15 +22,15 @@ contract DeployMintableToken is Script {
         bytes32 salt = vm.envOr("SALT", DEFAULT_DEPLOYMENT_SALT);
         address expectedAddress = vm.envOr("TOKEN_ADDRESS", address(0));
         address owner = vm.getWallets()[0];
+        bytes memory initCode =
+            abi.encodePacked(type(MintableERC20).creationCode, abi.encode(owner, name, symbol, decimals));
+
+        console2.log("MintableERC20 init code hash:");
+        console2.logBytes32(keccak256(initCode));
 
         vm.startBroadcast();
 
-        (address tokenAddress,) = deployIfNeeded(
-            abi.encodePacked(type(MintableERC20).creationCode, abi.encode(owner, name, symbol, decimals)),
-            salt,
-            expectedAddress,
-            symbol
-        );
+        (address tokenAddress,) = deployIfNeeded(initCode, salt, expectedAddress, symbol);
         token = MintableERC20(tokenAddress);
 
         vm.stopBroadcast();
